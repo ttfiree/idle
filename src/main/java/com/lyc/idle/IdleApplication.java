46e46025c65ef132cd8538a159a85d867fb5305f
ttfire <474497819@qq.com>
2023-05-30 18:16:01 +0800

    first

---
+++ src/main/java/com/lyc/idle/IdleApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.idle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdleApplication.class, args);
+    }
+
+}
